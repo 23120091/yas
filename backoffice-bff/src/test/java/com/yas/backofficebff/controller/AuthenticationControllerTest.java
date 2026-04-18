ce188b6df4c0a73810e58a5f2bd0a9ca1841fd26
Dang <gnadegreneyugn@gmail.com>
2026-04-18 12:45:02 +0700

    thêm unit test cho service backoffice

--- backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
+++ backoffice-bff/src/test/java/com/yas/backofficebff/controller/AuthenticationControllerTest.java
@@ -44,4 +44,24 @@ class AuthenticationControllerTest {
         mockMvc.perform(get("/authentication/user"))
                 .andExpect(status().isUnauthorized());
     }
+
+    @Test
+    void user_WhenPreferredUsernameIsNull_ShouldReturnNullUsername() {
+        // Giả lập trường hợp User đăng nhập nhưng không có thuộc tính preferred_username
+        when(principal.getAttribute("preferred_username")).thenReturn(null);
+
+        ResponseEntity<AuthenticatedUser> response = authenticationController.user(principal);
+
+        assertNotNull(response.getBody());
+        assertNull(response.getBody().username());
+    }
+
+    @Test
+    void user_WhenPreferredUsernameIsEmpty_ShouldReturnEmptyUsername() {
+        when(principal.getAttribute("preferred_username")).thenReturn("");
+
+        ResponseEntity<AuthenticatedUser> response = authenticationController.user(principal);
+
+        assertEquals("", response.getBody().username());
+    }
 }
