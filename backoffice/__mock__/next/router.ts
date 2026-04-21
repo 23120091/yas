da719178175fd007a5c6b4283b1ae3b92f33596c
HathUan14 <hazzthuan@gmail.com>
2026-04-22 01:54:56 +0700

    Im tired 2

---
+++ backoffice/__mock__/next/router.ts
@@ -0,0 +1,11 @@
+export const useRouter = () => ({
+  route: '/',
+  pathname: '/',
+  query: {},
+  asPath: '/',
+  push: jest.fn(),
+  replace: jest.fn(),
+  reload: jest.fn(),
+  back: jest.fn(),
+  prefetch: jest.fn().mockResolvedValue(null),
+});
--- backoffice/__tests__/home.test.tsx
+++ backoffice/__tests__/home.test.tsx
@@ -1,3 +1,4 @@
+jest.mock('next/router', () => require('../__mocks__/next/router'));
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
