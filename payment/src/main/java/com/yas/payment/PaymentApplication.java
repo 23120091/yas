46c7e1c1ad6c8943aebecc3ed0d0c4aefcdcb79f
Trần Thanh Thông <thongtran.dev@gmail.com>
2026-06-28 16:07:54 +0700

    fix: payment

--- payment/src/main/java/com/yas/payment/PaymentApplication.java
+++ payment/src/main/java/com/yas/payment/PaymentApplication.java
@@ -3,12 +3,14 @@ package com.yas.payment;
 import com.yas.commonlibrary.config.CorsConfig;
 import com.yas.payment.config.ServiceUrlConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-@SpringBootApplication
+@SpringBootConfiguration
+@EnableAutoConfiguration
 @ComponentScan(
     basePackages = {"com.yas.payment", "com.yas.commonlibrary"},
     excludeFilters = @ComponentScan.Filter(
